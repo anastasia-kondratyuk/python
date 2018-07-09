bfd309c754dc1653e6cc8fcb07df3d9571b2b437
anastasia-kondratyuk <kondratyuk.anastasia@stud.onu.edu.ua>
2018-07-09 22:13:09 +0300

    Задание 3. Исправлен неверный синтаксис оператора print

--- task3.py
+++ task3.py
@@ -1,11 +1,12 @@
-import sys , random
+import sys, random
 
-def compute ( n ):
-    i = 0; s = 0
-while i <= n:
-    s += random.random ()
-    i += 1
-return s /n
+def compute(n):
+    i = 0;
+    s = 0
+    while i <= n:
+        s += random.random()
+        i += 1
+    return s / n
 
-n = sys . argv [1]
-print ’average of %d random numbers is %g " % (n , compute (n ))
+n = sys.argv[1]
+print("average of %d random numbers is %g " % (n, compute(n)))
