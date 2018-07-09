ea3fd1aae42f3d2bf2deea269f12870daf8e6f54
anastasia-kondratyuk <kondratyuk.anastasia@stud.onu.edu.ua>
2018-07-09 22:29:58 +0300

    Задание 4. Четвертый вид цикла.

--- task4.py
+++ task4.py
@@ -2,9 +2,12 @@ from math import *
 from sys import *
 
 n = 1
-while n < len(argv):
-    if float(argv[n]) > 0:
-        print('ln(%g)' % float(argv[n]), " = ", log(float(argv[n])))
-    else:
-        print("ln(%g)" % float(argv[n]), " is illegal")
-    n += 1
+while 1:
+    try:
+        if float(argv[n]) > 0:
+            print("ln(%g)" % float(argv[n]), " = ", log(float(argv[n])))
+        else:
+            print("ln(%g)" % float(argv[n]), " is illegal")
+        n += 1
+    except:
+        break
